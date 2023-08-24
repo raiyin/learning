04d46c58cf72171175b47d95fc0e4aa608fdb1cb
Maxim P <raiyin@ya.ru>
2023-08-24 21:38:43 +0300

    Links and layouts

---
+++ nextjs/dojo-helpdesk/app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Navbar() {
+    return (<nav>
+        <h1>
+            Dijo Helpdesk
+        </h1>
+        <Link href="/">
+            Dashboard
+        </Link>
+        <Link href="/tickets">
+            Tickets
+        </Link>
+    </nav>
+    );
+}
--- nextjs/dojo-helpdesk/app/layout.jsx
+++ nextjs/dojo-helpdesk/app/layout.jsx
@@ -1,6 +1,9 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 
+// components.
+import Navbar from './components/Navbar';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -12,6 +15,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
+                <Navbar />
                 {children}
             </body>
         </html>
