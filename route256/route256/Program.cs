d128c0fe86766cc1949f6ee16c40b1cae71cb7f2
Maxim P <raiyin@ya.ru>
2023-08-26 21:25:04 +0300

    Pay sum route256

--- route256/route256/Program.cs
+++ route256/route256/Program.cs
@@ -7,18 +7,39 @@ namespace route256
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             int count = int.Parse(Console.ReadLine());
-            List<string> list = new List<string>();
+            List<Dictionary<int, int>> prices = new List<Dictionary<int, int>>();
+
             for (int i = 0; i < count; i++)
             {
-                list.Add(Console.ReadLine());
+                int pricesCount = int.Parse(Console.ReadLine());
+                prices.Add(new Dictionary<int, int>());
+                var parts = Console.ReadLine().Split(new char[] { ' ' }).Select(item => int.Parse(item));
+                foreach (var part in parts)
+                {
+                    if (!prices[i].ContainsKey(part))
+                    {
+                        prices[i].Add(part, 1);
+                    }
+                    else
+                    {
+                        prices[i][part]++;
+                    }
+                }
             }
 
             for (int i = 0; i < count; i++)
             {
-                Console.WriteLine(list[i].Split(new char[] { ' ' }).Select(item=>int.Parse(item)).Sum());
+                int sum = 0;
+                foreach (var key in prices[i].Keys)
+                {
+                    sum += prices[i][key] / 3 * 2 * key + (prices[i][key] % 3) * key;
+                }
+                Console.WriteLine(sum);
             }
         }
     }
