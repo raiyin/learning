22d3eb735ffbeb3d5360d84795336b9bf26cf99f
Maxim P <raiyin@ya.ru>
2023-08-26 20:10:27 +0300

    Styles, fonts and images

--- nextjs/dojo-helpdesk/app/components/Navbar.jsx
+++ nextjs/dojo-helpdesk/app/components/Navbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Logo from './dojo-logo.png';
 
 export default function Navbar() {
-    return (<nav>
-        <h1>
-            Dijo Helpdesk
-        </h1>
-        <Link href="/">
-            Dashboard
-        </Link>
-        <Link href="/tickets">
-            Tickets
-        </Link>
-    </nav>
+    return (
+        <nav>
+            <Image
+                src={Logo}
+                alt='Dojo Helpdesk logo'
+                width={70}
+                quality={100}
+                placeholder='blur'
+            />
+            <h1>
+                Dijo Helpdesk
+            </h1>
+            <Link href="/">
+                Dashboard
+            </Link>
+            <Link href="/tickets">
+                Tickets
+            </Link>
+        </nav>
     );
 }
