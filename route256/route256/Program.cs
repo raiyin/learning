d1ce4329d123045be7f1ce67a55db1113d58a974
Maxim P <raiyin@ya.ru>
2023-08-26 20:47:09 +0300

    Init and first task in route256

---
+++ route256/route256/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace route256
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(list[i].Split(new char[] { ' ' }).Select(item=>int.Parse(item)).Sum());
+            }
+        }
+    }
+}
