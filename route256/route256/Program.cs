bda20cbbe6a599708235b22e8e1a0a1224cb9695
Maxim P <raiyin@ya.ru>
2023-08-27 16:38:33 +0300

    Cards route256

--- route256/route256/Program.cs
+++ route256/route256/Program.cs
@@ -9,6 +9,36 @@ namespace route256
     {
         static void Main(string[] args)
         {
+            var inParams = Console.ReadLine().Split(new char[] { ' ' });
+            int fCount = int.Parse(inParams[0]);
+            int cCount = int.Parse(inParams[1]);
+
+            var friends = Console.ReadLine().Split(new char[] { ' ' }).Select(item => int.Parse(item)).ToList();
+            var cards = new List<int>();
+            cards.AddRange(Enumerable.Range(1, cCount));
+
+            int[] indexes = new int[fCount];
+
+            while (friends.Any(item => item != -1))
+            {
+                int smallest = friends.Where(item => item != -1).Min();
+                int index = friends.IndexOf(smallest);
+                friends[index] = -1;
+
+                if (cards.Any(item => item >= smallest + 1))
+                {
+                    var cSmallest = cards.Where(item => item >= (smallest + 1)).Min();
+                    cards.Remove(cSmallest);
+                    indexes[index] = cSmallest;
+                }
+                else
+                {
+                    Console.WriteLine("-1");
+                    return;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", indexes));
         }
     }
 }
