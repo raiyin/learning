0f8cff9ae0c5611f069759f1148046fa485b4184
Maxim P <raiyin@ya.ru>
2023-08-28 23:58:09 +0300

    Fetching and revalidating data

--- nextjs/dojo-helpdesk/app/tickets/page.jsx
+++ nextjs/dojo-helpdesk/app/tickets/page.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import TicketList from './TicketList';
 
 export default function Tickets() {
     return (
         <main>
-            <h2>
-                Tickets
-            </h2>
+            <nav>
+                <h2>Tickets</h2>
+                <p>
+                    <small>
+                        Currently open tickets.
+                    </small>
+                </p>
+            </nav>
+
+            <TicketList />
         </main>
     );
 }
