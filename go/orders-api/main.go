d705b5349ec4e076d93aafd02f047b7b913d2f7b
Maxim P <raiyin@ya.ru>
2023-09-14 16:13:14 +0300

    App created

--- go/orders-api/main.go
+++ go/orders-api/main.go
@@ -9,22 +9,22 @@ import (
 )
 
 func main() {
-	router:=chi.NewRouter()
+	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
 	router.Get("/hello", basicHandler)
 
-	server:=&http.Server{
-		Addr: ":3000",
+	server := &http.Server{
+		Addr:    ":3000",
 		Handler: router,
 	}
 
-	err:=server.ListenAndServe()
-	if err!=nil {
+	err := server.ListenAndServe()
+	if err != nil {
 		fmt.Println("Failed to listen to server", err)
 	}
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request){
+func basicHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world!"))
 }
