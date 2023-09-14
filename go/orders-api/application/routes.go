7783cd40ab9a937f9ec44f0e7cf234736759ba6c
Maxim P <raiyin@ya.ru>
2023-09-14 16:22:11 +0300

    Router added

---
+++ go/orders-api/application/routes.go
@@ -0,0 +1,36 @@
+package application
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func loadRoutes() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	return router
+}
+
+func (a *App) Start(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: a.router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+
+	return nil
+}
