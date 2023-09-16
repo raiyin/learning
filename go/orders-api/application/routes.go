c7efb5ca3adb72827b3b204eda2f1ecaf7f1a803
Maxim P <raiyin@ya.ru>
2023-09-16 17:56:01 +0300

    Graceful shutdown complete

--- go/orders-api/application/routes.go
+++ go/orders-api/application/routes.go
@@ -1,8 +1,6 @@
 package application
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/dreamsofcode-io/orders-api/handler"
@@ -21,20 +19,6 @@ func loadRoutes() *chi.Mux {
 
 	router.Route("/orders", loadOrderRoutes)
 	return router
-}
-
-func (a *App) Start(ctx context.Context) error {
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: a.router,
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
-	}
-
-	return nil
 }
 
 func loadOrderRoutes(router chi.Router) {
