842c2c4b1f9bac63ca1f7d1237b967f0425e7137
Maxim P <raiyin@ya.ru>
2023-11-15 23:30:57 +0300

    HtmlWebpackPlugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path') // path module import
+const HtmlWebpackPlugin = require('html-webpack-plugin') // to dynamicaly insert builded js-file in the index.html
 
 module.exports = (env) => { // чтобы настраивать сборку с помощью параметров
     return {
@@ -8,6 +9,9 @@ module.exports = (env) => { // чтобы настраивать сборку с
             path: path.resolve(__dirname, 'build'),
             filename: '[name].[contenthash].js', // dynamic build name, to not to cache build files
             clean: true, // delete old build, because browser cache files
-        }
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }) // будет подставлять файл сборки в html файл, tempalate нужен, так как иначе будет создаваться html-файл по умолчанию
+        ]
     }
 }
