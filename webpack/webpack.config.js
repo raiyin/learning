910c842210a7fc64c951d59cb6f4cd8f942e351b
Maxim P <raiyin@ya.ru>
2023-11-15 23:05:15 +0300

    Env vars for config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path') // path module import
 
-module.exports = {
-    mode: 'development', // 'production'
-    entry: path.resolve(__dirname, 'src', 'index.js'), // __dirname - current dir, entry - app entry point, there may be several of them
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].[contenthash].js', // dynamic build name, to not to cache build files
-        clean: true, // delete old build, because browser cache files
+module.exports = (env) => { // чтобы настраивать сборку с помощью параметров
+    return {
+        mode: env.mode ?? 'development', // 'production'
+        entry: path.resolve(__dirname, 'src', 'index.js'), // __dirname - current dir, entry - app entry point, there may be several of them
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js', // dynamic build name, to not to cache build files
+            clean: true, // delete old build, because browser cache files
+        }
     }
 }
