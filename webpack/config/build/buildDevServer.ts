54a42988903bf646b5b86ccba6646f725733040f
Maxim P <raiyin@ya.ru>
2023-11-23 23:19:16 +0300

    Routing, lazy, bundle size

--- webpack/config/build/buildDevServer.ts
+++ webpack/config/build/buildDevServer.ts
@@ -4,6 +4,8 @@ import { BuildOptions } from './types/types';
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port ?? 3000,
-        open: true
+        open: true,
+        // если раздавать статику через nginx, то надо делать проксирование на index.html
+        historyApiFallback: true
     }
 }
--- webpack/src/index.tsx
+++ webpack/src/index.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './components/App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { LazyAbout } from './pages/about/About.lazy'
+import { Shop } from './pages/Shop'
+import { Suspense } from 'react'
 
 const root = document.getElementById('root')
 
@@ -8,4 +12,24 @@ if (!root) {
 }
 
 const container = createRoot(root)
-container.render(<App />)
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: '/about',
+                element: <Suspense fallback={'loading...'}><LazyAbout /></Suspense>
+            },
+            {
+                path: '/shop',
+                element: <Suspense fallback={'loading...'}><Shop /></Suspense>
+            }
+        ]
+    },
+]);
+
+container.render(
+    <RouterProvider router={router} />
+)
