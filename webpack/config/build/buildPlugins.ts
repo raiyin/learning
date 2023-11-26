550c074d5d08e8cc64e51d1a8da2e96352ad7298
Maxim P <raiyin@ya.ru>
2023-11-26 18:19:58 +0300

    Copy plugin

--- webpack/config/build/buildPlugins.ts
+++ webpack/config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import { BuildOptions } from "./types/types";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin"
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin"
+import CopyPlugin from "copy-webpack-plugin"
 import path from "path";
 
 export function buildPlugins({ mode, paths, analyzer, platform }: BuildOptions): Configuration['plugins'] {
@@ -34,6 +35,11 @@ export function buildPlugins({ mode, paths, analyzer, platform }: BuildOptions):
         plugins.push(new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
+        }))
+        plugins.push(new CopyPlugin({
+            patterns: [
+                { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
+            ]
         }))
     }
 
